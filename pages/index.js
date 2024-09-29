f31a5e91834de921e594987b2507fbe057ba1c14
Volodymyr Svirhun <volodymyr.svirhun@awesomic.design>
2024-09-29 02:15:15 +0200

    hello world

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      hello world
     </div>
   );
 }
